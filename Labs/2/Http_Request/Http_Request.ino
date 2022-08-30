4ae9e2ac080a594982b383efeded8351ac94490e
hughjazzman <wira.azmoon@gmail.com>
2022-08-30 17:54:34 +0800

    [L2] Complete

--- Labs/2/Http_Request/Http_Request.ino
+++ Labs/2/Http_Request/Http_Request.ino
@@ -1,7 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 #include "config.h"
+
+#define DHTPIN D6    // Digital pin connected to the DHT sensor 
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
 
 const char* ssid = SSID; //Your Wifi's SSID
 const char* password = PASSWORD; //Wifi Password
@@ -9,8 +19,72 @@ const char* password = PASSWORD; //Wifi Password
 WiFiClient wifiClient;
 const char* laptopAt = LAPTOP_IP; //change to your Laptop's IP
 
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+
+float temp, humid;
+
+void setup_dht() {
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+}
+
+void update_vals() {
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+    temp = event.temperature;
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+    humid = event.relative_humidity;
+  }
+}
+
 void setup(void){
   Serial.begin(115200);
+
+  setup_dht();
+
   WiFi.begin(ssid, password);
   Serial.println("");
   
@@ -27,13 +101,13 @@ void setup(void){
 }
 
 void loop(void){
+  update_vals();
   Serial.print("Sending...");
   if (WiFi.status() == WL_CONNECTED){
     HTTPClient http;
     
     String url = laptopAt;
-    url += "data?data1=31.2&data2=76"; //hardcoded values for example only
-    
+    url += "data?data1=" + String(temp) + "&data2=" + String(humid);
     http.begin(wifiClient,url);
     int returnCode = http.GET();   //perform a HTTP GET request
     
